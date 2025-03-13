f78c83f6f7d13c2db9be618fbfef8edab6bba6df
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-03-13 14:50:50 +0100

    Update entrypoint.sh

--- charts/srsran-5g-zmq-cu/resources/entrypoint.sh
+++ charts/srsran-5g-zmq-cu/resources/entrypoint.sh
@@ -2,20 +2,25 @@
 
 # set -ex
 
-if [ $# -lt 1 ]
-then
-        echo "Usage : $0 [gnb]"
-        exit
+
+resolve_ip() {
+    python3 -c "import socket; print(socket.gethostbyname('$1'))" 2>/dev/null
+}
+
+if [[ -n "$AMF_HOSTNAME" ]]; then 
+    export AMF_ADDR="$(resolve_ip "$AMF_HOSTNAME")"
 fi
 
-# if [[ ! -z "$AMF_HOSTNAME" ]] ; then 
-#     export AMF_ADDR="$(host -4 $AMF_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
-# fi
+if [[ -n "$SRSRAN_CU_GTPU" ]]; then 
+    export SRSRAN_CU_GTPU_IP="$(resolve_ip "$SRSRAN_CU_GTPU")"
+fi
 
-# if [[ -z "${AMF_BIND_ADDR}" ]] ; then
-#     export AMF_BIND_ADDR=$(ip addr show $AMF_BIND_INTERFACE | grep -Po 'inet \K[\d.]+')
-# fi
+if [[ -n "$SRSRAN_CU_F1AP" ]]; then 
+    export SRSRAN_CU_F1AP_IP="$(resolve_ip "$SRSRAN_CU_F1AP")"
+fi
 
-# envsubst < /gnb-template.yml > gnb.yml
-
-/usr/local/bin/srscu -c /cu-template.yml
+# Replace variables in the template
+sed -e "s/\${SRSRAN-CU-GTPU}/$SRSRAN_CU_GTPU_IP/g" \
+    -e "s/\${SRSRAN-CU-F1AP}/$SRSRAN_CU_F1AP_IP/g" \
+    -e "s/\${AMF_ADDR}/$AMF_ADDR/g" \
+    < /cu-template.yml > cu.yml
