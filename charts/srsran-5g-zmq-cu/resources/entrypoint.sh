55f0cedcf682a2e77dac5949e59f43ad6084b31d
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-03-13 15:11:02 +0100

    Update entrypoint.sh

--- charts/srsran-5g-zmq-cu/resources/entrypoint.sh
+++ charts/srsran-5g-zmq-cu/resources/entrypoint.sh
@@ -15,6 +15,10 @@ if [[ -n "$SRSRAN_CU_GTPU" ]]; then
     export SRSRAN_CU_GTPU_IP="$(resolve_ip "$SRSRAN_CU_GTPU")"
 fi
 
+if [[ -z "${AMF_BIND_ADDR}" ]] ; then
+    export AMF_BIND_ADDR=$(hostname -I)
+fi
+
 if [[ -n "$SRSRAN_CU_F1AP" ]]; then 
     export SRSRAN_CU_F1AP_IP="$(resolve_ip "$SRSRAN_CU_F1AP")"
 fi
@@ -22,6 +26,7 @@ fi
 # Replace variables in the template
 sed -e "s/\${SRSRAN-CU-GTPU}/$SRSRAN_CU_GTPU_IP/g" \
     -e "s/\${SRSRAN-CU-F1AP}/$SRSRAN_CU_F1AP_IP/g" \
+    -e "s/\${AMF_BIND_ADDR}/$AMF_BIND_ADDR/g" \    
     -e "s/\${AMF_ADDR}/$AMF_ADDR/g" \
     < /cu-template.yml > cu.yml
 
