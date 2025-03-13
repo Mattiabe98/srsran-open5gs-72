440fd8302433cd8552d34c5753805c350f12c0c5
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-03-13 17:31:31 +0100

    Update entrypoint.sh

--- charts/srsran-5g-zmq-du/resources/entrypoint.sh
+++ charts/srsran-5g-zmq-du/resources/entrypoint.sh
@@ -12,10 +12,12 @@ if [[ -z "$DU_BIND_ADDR" ]] ; then
 fi
 
 export CU_ADDR="$(resolve_ip "$CU_ADDRESS")"
+export METRICS_ADDR="$(resolve_ip "$METRICS_ADDRESS")"
 
 # Replace variables in the template
 sed -e "s/\${CU_ADDR}/$CU_ADDR/g" \
     -e "s/\${DU_BIND_ADDR}/$DU_BIND_ADDR/g" \
+    -e "s/\${METRICS_ADDR}/$METRICS_ADDR/g" \
     < /du-template.yml > du.yml
 
 /usr/local/bin/srsdu -c /du.yml
