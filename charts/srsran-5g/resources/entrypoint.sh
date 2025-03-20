8c219fa8c1b06ad315d7d96d5e21ad5f4205252c
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-03-20 15:46:11 +0100

    Update entrypoint.sh

--- charts/srsran-5g/resources/entrypoint.sh
+++ charts/srsran-5g/resources/entrypoint.sh
@@ -2,18 +2,16 @@
 
 set -ex
 
-if [ $# -lt 1 ]
-then
-        echo "Usage : $0 [gnb]"
-        exit
-fi
+resolve_ip() {
+    python3 -c "import socket; print(socket.gethostbyname('$1'))" 2>/dev/null
+}
 
-if [[ ! -z "$AMF_HOSTNAME" ]] ; then 
-    export AMF_ADDR="$(host -4 $AMF_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
+if [[ -n "$AMF_HOSTNAME" ]]; then 
+    export AMF_ADDR="$(resolve_ip "$AMF_HOSTNAME")"
 fi
 
 if [[ -z "${AMF_BIND_ADDR}" ]] ; then
-    export AMF_BIND_ADDR=$(ip addr show $AMF_BIND_INTERFACE | grep -Po 'inet \K[\d.]+')
+    export AMF_BIND_ADDR=$(hostname -I)
 fi
 
 if [[ ! -z "$GNB_HOSTNAME" ]] ; then 
@@ -29,6 +27,9 @@ fi
 
 
 
-envsubst < /gnb-template.yml > gnb.yml
+sed -e "s/\${AMF_BIND_ADDR}/$AMF_BIND_ADDR/g" \
+    -e "s/\${AMF_ADDR}/$AMF_ADDR/g" \
+    -e "s/\${E2_ADDR}/$E2_ADDR/g" \
+    < /gnb-template.yml > /gnb.yml
 
-/opt/srsRAN_Project/target/bin/gnb -c gnb.yml
+/usr/local/bin/gnb -c /gnb.yml
