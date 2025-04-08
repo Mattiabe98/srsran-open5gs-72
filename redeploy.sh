21b174f71dceda56c3ddf32da983724341242654
Super User <boing7898@gmail.com>
2025-04-08 11:04:03 +0200

    Wait for core to be up

--- redeploy.sh
+++ redeploy.sh
@@ -4,8 +4,11 @@ helm uninstall srsran-5g
 helm uninstall linuxptp
 helm uninstall influxdb2
 helm uninstall grafana-srsran
-sleep 2
+sleep 10
 git pull
+cd charts/open5gs/
+helm dependency update
+cd ../../
 helm install open5gs charts/open5gs
 #helm install linuxptp charts/linuxptp
 helm install influxdb2 charts/influxdb2
