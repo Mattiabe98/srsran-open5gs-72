28bfba784eb91ef5dc6d68ba13b59fb49198e1eb
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-04-19 18:14:47 +0200

    Update start-metrics.sh

--- charts/srsran-5g/resources/start-metrics.sh
+++ charts/srsran-5g/resources/start-metrics.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
 echo "Starting turbostat monitoring..."
-# Start turbostat in the background (write output to a file)
-turbostat -o /mnt/data/turbostat_output.txt;
+
+# Output file
+LOGFILE="/mnt/data/turbostat_output.txt"
+
+# Interval in seconds
+INTERVAL=5
+
+# Start logging loop
+while true; do
+    # Get current UTC time
+    UTC_TIME=$(date -u +"%Y-%m-%d %H:%M:%S")
+
+    # Write timestamp header
+    echo "[$UTC_TIME UTC]" >> "$LOGFILE"
+
+    # Run turbostat for one sample
+    turbostat --interval "$INTERVAL" >> "$LOGFILE"
+
+    # Optional: separator for readability
+    echo "----------------------------------------" >> "$LOGFILE"
+done
