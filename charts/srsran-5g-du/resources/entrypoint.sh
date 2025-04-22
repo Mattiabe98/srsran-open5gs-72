19d4ac9c77ca2396d1523f872c9b0af2cc2cb368
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-04-22 15:09:08 +0200

    Update entrypoint.sh

--- charts/srsran-5g-du/resources/entrypoint.sh
+++ charts/srsran-5g-du/resources/entrypoint.sh
@@ -61,3 +61,4 @@ ip route add 10.10.1.1/32 via 10.10.1.4 dev vpp1host
 # echo N | tee /sys/module/drm_kms_helper/parameters/poll >/dev/null
 # /opt/dpdk/23.11.1/bin/dpdk-devbind.py --bind vfio-pci 0000:51:11.0
 # stdbuf -oL -eL /usr/local/bin/srsdu -c /gnb.yml
+sleep infinity
