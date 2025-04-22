248c0ef91c4ffe3127a2fb9caf73868d2850308e
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-04-22 13:21:29 +0200

    Update entrypoint.sh

--- charts/srsran-5g-du/resources/entrypoint.sh
+++ charts/srsran-5g-du/resources/entrypoint.sh
@@ -34,13 +34,24 @@ sed -e "s/\${AMF_BIND_ADDR}/$AMF_BIND_ADDR/g" \
 vpp -c /etc/vpp/startup.conf &
 
 # Wait for VPP socket to exist so we can talk to it
-while [ ! -e /run/vpp/cli.sock ]; do
+while [ ! -e /run/vpp/cli-du.sock ]; do
     sleep 0.2
 done
 
-vppctl create interface memif id 0 socket-id 0 socket /run/memif/memif.sock slave
-vppctl set interface state memif0 up
-vppctl set interface ip address memif0 192.168.100.2/30
+ip link add name vpp1out type veth peer name vpp1host
+ip link set dev vpp1out  up
+ip link set dev vpp1host up
+ip addr add 10.10.1.3/24 dev vpp1host
+
+vppctl create host-interface name vpp1out
+vppctl set int state host-vpp1out up
+vppctl set int ip address host-vpp1out 10.10.1.4/24
+
+vppctl create memif socket id 1 filename /run/memif/memif.sock
+vppctl create interface memif id 0 socket-id 1 slave
+vppctl set int state memif0/0 up
+vppctl set int ip address memif0/0 10.10.2.2/24
+
 
 echo N | tee /sys/module/drm_kms_helper/parameters/poll >/dev/null
 /opt/dpdk/23.11.1/bin/dpdk-devbind.py --bind vfio-pci 0000:51:11.0
