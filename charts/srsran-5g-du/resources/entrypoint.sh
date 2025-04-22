3864f515d795612a8c13ae5e472261564073d26d
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-04-22 14:47:14 +0200

    Update entrypoint.sh

--- charts/srsran-5g-du/resources/entrypoint.sh
+++ charts/srsran-5g-du/resources/entrypoint.sh
@@ -54,7 +54,7 @@ vppctl -s /run/vpp/cli-du.sock set int ip address memif1/0 10.10.2.2/24
 
 ip route add 10.10.2.0/24 via 10.10.1.4
 
-vppctl -s /run/vpp/cli-du.sock ip route add 10.10.1.0/24 via 10.10.2.1
+vppctl -s /run/vpp/cli-du.sock ip route add 10.10.1.1/32 via 10.10.2.1 memif1/0
 
 echo N | tee /sys/module/drm_kms_helper/parameters/poll >/dev/null
 /opt/dpdk/23.11.1/bin/dpdk-devbind.py --bind vfio-pci 0000:51:11.0
