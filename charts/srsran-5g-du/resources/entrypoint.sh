e308cdb1ffaa4c7bf3d770c28e9e7bedebb207de
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-04-23 12:49:02 +0200

    Update entrypoint.sh

--- charts/srsran-5g-du/resources/entrypoint.sh
+++ charts/srsran-5g-du/resources/entrypoint.sh
@@ -6,6 +6,7 @@ resolve_ip() {
     python3 -c "import socket; print(socket.gethostbyname('$1'))" 2>/dev/null
 }
 
+# Resolve addresses
 if [[ -n "$AMF_HOSTNAME" ]]; then 
     export AMF_ADDR="$(resolve_ip "$AMF_HOSTNAME")"
 fi
@@ -18,19 +19,39 @@ if [[ ! -z "$GNB_HOSTNAME" ]] ; then
     export GNB_ADDRESS="$(resolve_ip "$GNB_HOSTNAME")"
 fi
 
-
 if [[ ! -z "$UE_HOSTNAME" ]] ; then 
     export UE_ADDRESS="$(resolve_ip "$UE_HOSTNAME")"
 fi
 
 export E2_ADDR=$(resolve_ip "$E2_HOSTNAME")
 
-
+# Generate config
 sed -e "s/\${AMF_BIND_ADDR}/$AMF_BIND_ADDR/g" \
     -e "s/\${AMF_ADDR}/$AMF_ADDR/g" \
     -e "s/\${E2_ADDR}/$E2_ADDR/g" \
     < /gnb-template.yml > /gnb.yml
 
+# Disable polling for display driver
 echo N | tee /sys/module/drm_kms_helper/parameters/poll >/dev/null
+
+# DPDK device bind
 /opt/dpdk/23.11.1/bin/dpdk-devbind.py --bind vfio-pci 0000:51:11.0
+
+# Start turbostat in the background
+(
+    echo "Starting turbostat monitoring..."
+    TIMESTAMP=$(date -u +"%Y-%m-%d_%H-%M-%S")
+    LOGFILE="/mnt/data/turbostat_output_du_$TIMESTAMP.txt"
+    INTERVAL=5
+
+    turbostat --interval "$INTERVAL" | while IFS= read -r line; do
+        if [[ "$line" =~ ^\ *- ]]; then
+            UTC_TIME=$(date -u +"%Y-%m-%d %H:%M:%S")
+            echo "[$UTC_TIME UTC]" >> "$LOGFILE"
+        fi
+        echo "$line" >> "$LOGFILE"
+    done
+) &
+
+# Launch srsDU
 stdbuf -oL -eL /usr/local/bin/srsdu -c /gnb.yml
