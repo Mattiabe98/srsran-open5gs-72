e95a342bdeccaabf06193745043eb5e185f1205c
root <boing7898@gmail.com>
2025-04-24 12:29:12 +0200

    redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 helm uninstall open5gs
-helm uninstall srsran-5g
-helm uninstall linuxptp
+helm uninstall srsran-5g-cu srsran-5g-du
 helm uninstall influxdb2
 helm uninstall grafana-srsran
 sleep 10
@@ -10,8 +9,5 @@ cd charts/open5gs/
 helm dependency update
 cd ../../
 helm install open5gs charts/open5gs
-#helm install linuxptp charts/linuxptp
 helm install influxdb2 charts/influxdb2
 helm install grafana-srsran charts/grafana-srsran
-echo "Waiting for gNB and carriers"
-echo "helm install srsran-5g charts/srsran-5g"
