3a37a34e75c08244b0ae4c90d4722d9d2cc72f98
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-04-30 15:58:58 +0200

    Update downclock.sh

--- charts/srsran-5g-du/resources/downclock.sh
+++ charts/srsran-5g-du/resources/downclock.sh
@@ -4,13 +4,20 @@ START_FREQ=3500000  # in kHz
 END_FREQ=1200000    # in kHz
 STEP=50000          # in kHz
 INTERVAL=30         # in seconds
+LOGFILE="downclock_log.txt"
 
 current_freq=$START_FREQ
+
+# Clear or create the log file
+echo "=== Downclocking started at $(date) ===" > "$LOGFILE"
 
 # Function to apply frequency to all logical CPUs
 set_all_cpus_freq() {
   local freq=$1
-  echo "Setting scaling_max_freq to $((freq / 1000)) MHz..."
+  local mhz=$((freq / 1000))
+  local timestamp=$(date '+%F %T')
+  echo "[$timestamp] Setting scaling_max_freq to ${mhz} MHz..." | tee -a "$LOGFILE"
+  
   for cpu_path in /sys/devices/system/cpu/cpu[0-9]*; do
     freq_file="$cpu_path/cpufreq/scaling_max_freq"
     if [[ -f $freq_file ]]; then
@@ -27,7 +34,7 @@ while (( current_freq >= END_FREQ )); do
 done
 
 # Wait, then restore original frequency
-echo "Reached $((END_FREQ / 1000)) MHz. Waiting $INTERVAL seconds before restoring..."
+echo "[$(date '+%F %T')] Reached ${END_FREQ/000/} MHz. Waiting $INTERVAL seconds before restoring..." | tee -a "$LOGFILE"
 sleep $INTERVAL
 set_all_cpus_freq $START_FREQ
-echo "Restored to $((START_FREQ / 1000)) MHz."
+echo "[$(date '+%F %T')] Restored to ${START_FREQ/000/} MHz." | tee -a "$LOGFILE"
