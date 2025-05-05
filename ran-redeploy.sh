19f79c57bfb8d8e962e58e887069f80df631eb85
root <boing7898@gmail.com>
2025-05-05 18:02:34 +0200

    updated

--- ran-redeploy.sh
+++ ran-redeploy.sh
@@ -1,6 +1,12 @@
 #/bin/bash
 git pull
 helm uninstall srsran-5g-cu
+helm uninstall srsran-5g-du
+helm uninstall srsran-5g-du-1
 helm uninstall srsran-5g-du-2
+
+sleep 5
 helm install srsran-5g-cu charts/srsran-5g-cu
+helm install srsran-5g-du charts/srsran-5g-du
+helm install srsran-5g-du-1 charts/srsran-5g-du-1
 helm install srsran-5g-du-2 charts/srsran-5g-du-2
