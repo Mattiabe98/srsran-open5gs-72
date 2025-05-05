8d015e7f471550f03030ed5ef914528d75df31dd
root <boing7898@gmail.com>
2025-05-05 18:13:38 +0200

    a

--- ran-redeploy.sh
+++ ran-redeploy.sh
@@ -7,6 +7,9 @@ helm uninstall srsran-5g-du-2
 
 sleep 5
 helm install srsran-5g-cu charts/srsran-5g-cu
+sleep 1
 helm install srsran-5g-du charts/srsran-5g-du
+sleep 1
 helm install srsran-5g-du-1 charts/srsran-5g-du-1
+sleep 1
 helm install srsran-5g-du-2 charts/srsran-5g-du-2
