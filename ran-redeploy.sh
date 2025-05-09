5907ea56b0c53bc875904dd60a7c6043034c7648
root <boing7898@gmail.com>
2025-05-09 10:59:07 +0200

    6 DU

--- ran-redeploy.sh
+++ ran-redeploy.sh
@@ -4,6 +4,9 @@ helm uninstall srsran-5g-cu
 helm uninstall srsran-5g-du
 helm uninstall srsran-5g-du-1
 helm uninstall srsran-5g-du-2
+helm uninstall srsran-5g-du-3
+helm uninstall srsran-5g-du-4
+helm uninstall srsran-5g-du-5
 
 sleep 5
 helm install srsran-5g-cu charts/srsran-5g-cu
@@ -13,3 +16,9 @@ sleep 1
 helm install srsran-5g-du-1 charts/srsran-5g-du-1
 sleep 1
 helm install srsran-5g-du-2 charts/srsran-5g-du-2
+sleep 1
+helm install srsran-5g-du-3 charts/srsran-5g-du-3
+sleep 1
+helm install srsran-5g-du-4 charts/srsran-5g-du-4
+sleep 1
+helm install srsran-5g-du-5 charts/srsran-5g-du-5
