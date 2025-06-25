a90d9f7306f45ca23202ce5227c40a588f38bb02
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-06-25 17:20:10 +0200

    Update ran-redeploy-all.sh

--- ran-redeploy-all.sh
+++ ran-redeploy-all.sh
@@ -3,7 +3,7 @@ git pull
 helm uninstall srsran-5g-cu
 helm uninstall srsran-5g-du srsran-5g-du-1 srsran-5g-du-3 srsran-5g-du-4
 helm install srsran-5g-cu charts/srsran-5g-cu
-helm install srsran-5g-du-4 charts/srsran-5g-du-4
+helm install srsran-5g-du charts/srsran-5g-du
 helm install srsran-5g-du-1 charts/srsran-5g-du-1
 helm install srsran-5g-du-3 charts/srsran-5g-du-3
-helm install srsran-5g-du charts/srsran-5g-du
+helm install srsran-5g-du-4 charts/srsran-5g-du-4
